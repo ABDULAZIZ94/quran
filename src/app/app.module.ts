07841877157699d21296af0ecc766a0263ee71da
abdulazizabdulnoor@gmail.com <abdulazizabdulnoor@gmail.com>
2020-04-09 06:22:25 +0800

    working firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select'; 
 
+
 @NgModule({
   declarations: [
     AppComponent,
