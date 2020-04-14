4ac5683a47a87635696165db5c1972bceb8931bf
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-04-15 00:40:57 +0800

    added firebase config to production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCMQdiNEzGmedSlh-EHhtSCFi7W1ZVofos",
+    authDomain: "loyal-channel-250108.firebaseapp.com",
+    databaseURL: "https://loyal-channel-250108.firebaseio.com",
+    projectId: "loyal-channel-250108",
+    storageBucket: "loyal-channel-250108.appspot.com",
+    messagingSenderId: "928857578335",
+    appId: "1:928857578335:web:e263a297eecb552efc74ea",
+    measurementId: "G-HSHY9NSZDC"
+  }
 };
